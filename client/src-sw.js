9080cbe0657035b83bdd440293848deb41c9d700
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-01 22:57:46 -0400

    Implement asset caching for src-sw.js added

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,4 +28,18 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
 
-registerRoute(({ request }) => []);
+registerRoute(
+  // only returns true if request is these types: style, script, worker
+  ({ request }) => ["style", "script", "worker"].includes(request.destribution),
+  // tries to serve the response from cache, but also fetch a fresh copy and update the cache in background
+  new StaleWhileRevalidate({
+    // use a seperate cache (cacheName) to store the response of this route
+    cacheName: "asset-cache",
+    plugins: [
+      // ensures only valid responses are chached
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
